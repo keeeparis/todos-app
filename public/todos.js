f4b964f068218e9e4b69535f2305a59ea797b8ac
Keeeparis <vladimir1trotsenko@gmail.com>
2021-08-29 15:21:02 +0600

    archiv -> searching, deleting

--- public/todos.js
+++ public/todos.js
@@ -4,13 +4,16 @@ try {
     const listItems = document.querySelectorAll('.list-group-item.to-update > p')
     const clearAllButton = document.querySelector('#clearAll')
     const clearDoneButton = document.querySelector('#clearDone')
-    const ulLists = document.querySelectorAll('.list-group')
+    const ulLists = document.querySelectorAll('.list-group.auto-show')
+    const formArchiv = document.querySelector('#form-archiv')
+
 
     listItemsButtons.forEach(el => el.addEventListener('click', closeToDo))
     listItems.forEach(el => el.addEventListener('click', markAsDone))
     form.addEventListener('submit', ShowTodoList)
     clearAllButton.addEventListener('click', clearAll)
     clearDoneButton.addEventListener('click', clearDone)
+    formArchiv.addEventListener('submit', showArchiv)
 
     async function ShowTodoList(evt) {
         evt.preventDefault()
@@ -92,6 +95,63 @@ try {
             ul.previousSibling.style.display = 'block'
         }
     })
+
+    async function showArchiv(event) {
+        event.preventDefault()
+
+        const from = document.querySelector('#from').value
+        const to = document.querySelector('#to').value
+        
+        const archiv = await fetch('/features/archiv', {
+            method: 'POST',
+            body: JSON.stringify({
+                'from' : from,
+                'to' : to
+            }),
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        }).then((res) => {
+            return res.json()
+        }).then((archiv) => {
+            showArchivList(archiv)
+        })
+    }
+
+    function showArchivList(data) {
+        const archiv = document.querySelector('#archiv')
+        let out = `<ul class='list-group list-archiv my-4'>`
+        for (let i=0; i<data.length; i++) {
+            out += `<li class='list-group-item to-archiv'>
+            <p> ${i+1}. ${data[i]['text']} ----- <span><b>done</b> 
+            ${new Date(Date.parse(data[i]['date'])).toLocaleString()}</span></p></li>`
+        }
+        out += `</ul>`
+        out += `<div class='delete-archiv-precautions'><button class='btn btn-outline-danger' id='archiv-delete'>Delete all archiv todos</button>
+        <p><b>Note</b>: this action is irreversible!</p>`
+        archiv.innerHTML = out
+        const deleteArchiv = document.querySelector('#archiv-delete')
+        deleteArchiv.addEventListener('click', deleteAllArchiv)
+        showDeleteArchivButton()
+    }
+
+    async function deleteAllArchiv() {
+        await fetch('/features/archiv-delete', {
+            method: 'POST'
+        })
+        window.location.href = '/features'
+    }
+
+    function showDeleteArchivButton() {
+        let archiv = document.querySelector('#archiv')
+        let div = document.querySelector('.delete-archiv-precautions')
+        if (archiv.firstChild.children.length == 0) {
+            div.style.display = 'none'
+        } else {
+            div.style.display = 'block'
+        }
+    }
 
 
 } catch (e) {
